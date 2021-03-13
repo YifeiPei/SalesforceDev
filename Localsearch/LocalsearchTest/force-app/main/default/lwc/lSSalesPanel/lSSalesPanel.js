7e95f9532ea667810b4b0899d02253b285d0b026
Yifei Pei <YifeiPei@users.noreply.github.com>
2021-03-13 21:01:48 +1030

    sorting function

--- Localsearch/LocalsearchTest/force-app/main/default/lwc/lSSalesPanel/lSSalesPanel.js
+++ Localsearch/LocalsearchTest/force-app/main/default/lwc/lSSalesPanel/lSSalesPanel.js
@@ -3,10 +3,10 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getSalesInfo from '@salesforce/apex/LSSalesPanelCtrl.getData';
 
 const columns = [
-    { label: 'Business Name', fieldName: 'businessName' },
-    { label: 'Type', fieldName: 'type' },
-    { label: 'Status', fieldName: 'status' },
-    { label: 'Due Date', fieldName: 'dueDate', type: 'date' },
+    { label: 'Business Name', fieldName: 'businessName', sortable: "true" },
+    { label: 'Type', fieldName: 'type', sortable: "true" },
+    { label: 'Status', fieldName: 'status', sortable: "true" },
+    { label: 'Due Date', fieldName: 'dueDate', type: 'date', sortable: "true" },
 ];
 
 export default class LSSalesPanel extends LightningElement {
@@ -14,6 +14,8 @@ export default class LSSalesPanel extends LightningElement {
     @track data = [];
     @track error;
     @track columns = columns;
+    @track sortBy;
+    @track sortDirection;
 
     @wire(getSalesInfo)
     wiredResponse(result) {
@@ -28,5 +30,34 @@ export default class LSSalesPanel extends LightningElement {
         }
         console.log('data', this.data);
         console.log('error', this.error);
+    }
+
+    handleSortdata(event) {
+        // field name
+        this.sortBy = event.detail.fieldName;
+        // sort direction
+        this.sortDirection = event.detail.sortDirection;
+        // calling sortdata function to sort the data based on direction and selected field
+        this.sortData(event.detail.fieldName, event.detail.sortDirection);
+    }
+
+    sortData(fieldname, direction) {
+        // serialize the data before calling sort function
+        let parseData = JSON.parse(JSON.stringify(this.data));
+        // Return the value stored in the field
+        let keyValue = (a) => {
+            return a[fieldname];
+        };
+        // cheking reverse direction 
+        let isReverse = direction === 'asc' ? 1 : -1;
+        // sorting data 
+        parseData.sort((x, y) => {
+            x = keyValue(x) ? keyValue(x) : ''; // handling null values
+            y = keyValue(y) ? keyValue(y) : '';
+            // sorting values based on direction
+            return isReverse * ((x > y) - (y > x));
+        });
+        // set the sorted data to data table data
+        this.data = parseData;
     }
 }
